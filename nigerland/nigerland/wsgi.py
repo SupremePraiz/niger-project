d26ca2f0297c0f68885df83898f7d812d0d30832
Supremepraiz <supremepraiz2@gmail.com>
2024-04-11 20:23:55 +0100

    settings wsgi

--- nigerland/nigerland/wsgi.py
+++ nigerland/nigerland/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nigerland.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nigerland.settings')
 
+os.environ["DJANGO_SETTINGS_MODULE"] = 'nigerland.settings'
 application = get_wsgi_application()
