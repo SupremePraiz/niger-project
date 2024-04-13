89841b791030c9977138b77f951bf8132d760702
Supremepraiz <supremepraiz2@gmail.com>
2024-04-13 01:41:42 +0100

    commented codes in build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 pip install -r requirements.txt
 
 
-python manage.py collectstatic --no-input
-py manage.py migrate
+# python manage.py collectstatic --no-input
+# py manage.py migrate
 
