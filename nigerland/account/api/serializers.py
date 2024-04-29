be6e5270d4d906f5c3abcca5b1a1f767d8858958
Jude Adeyemi <121219383+SupremePraiz@users.noreply.github.com>
2024-04-29 18:04:34 +0100

    Update serializers.py

--- nigerland/account/api/serializers.py
+++ nigerland/account/api/serializers.py
@@ -1,27 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class RegistrationSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={"input_type":"password"}, write_only=True)
+    confirm_password = serializers.CharField(style={"input_type":"password"}, write_only=True)
+    password = serializers.CharField(style={"input_type":"password"})
     
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2"]
+        fields = ["username", "email", "password", "confirm_password"]
         extra_kwargs ={
-            "password":{"write_only": True}
+            "password":{"write_only": True},
+            
         }
         
     def save(self):
-        passsword = self.validated_data["password"]
-        passsword2 = self.validated_data["password2"]
+        password = self.validated_data["password"]
+        confirm_password = self.validated_data["confirm_password"]
             
-        if passsword != passsword2:
+        if password != confirm_password:
             raise serializers.ValidationError({"error":"password1 and password2 must be the same"})
         if User.objects.filter(email=self.validated_data["email"]).exists():
             raise serializers.ValidationError({"error":"this email has already been used"})
             
         account =User(email=self.validated_data["email"], username=self.validated_data["username"])
-        account.set_password(passsword)
+        account.set_password(password)
         account.save()
             
         return account
